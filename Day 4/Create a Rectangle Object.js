bdcc5231c8ef6b2941e8020091a80ce969df985b
anuragc10 <63997049+anuragc10@users.noreply.github.com>
2021-04-15 16:04:00 +0530

    Create Create a Rectangle Object.js

---
+++ Day 4/Create a Rectangle Object.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    this.length=a;
+    this.width=b;
+    this.area=Number(a)*Number(b);
+    this.perimeter=2*(Number(a)+Number(b));
+    
+}
