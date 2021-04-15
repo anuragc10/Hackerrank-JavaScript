08a782b6d2dbabbc3593e41453d19711d87eac83
anuragc10 <63997049+anuragc10@users.noreply.github.com>
2021-04-15 15:45:22 +0530

    Create Throw.js

---
+++ Day 3/Throw.js
@@ -0,0 +1,13 @@
+function isPositive(a) {
+    if(a>0)
+    {
+        return "YES";
+    }
+    else if(a<0){
+        return "Negative Error";
+    }
+    else if(a===0){
+        return "Zero Error";
+    }
+    
+}
